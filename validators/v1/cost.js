02deda74f3252597dfdf88aa74451da59977e264
nazi <nazipaeez777@gmail.com>
2024-01-21 22:41:09 +0330

    Develope costs validator

---
+++ validators/v1/cost.js
@@ -0,0 +1,15 @@
+const validator = require('fastest-validator')
+
+const v = new validator()
+
+const schema = {
+    title: { type: 'string', min: 3, max: 100 },
+    status: { type: 'enum', values: ['پرداخت شده', 'در انتظار پرداخت', 'لغو شده'] },
+    date: { type: 'string' },
+    price: { type: 'number' },
+    $$strict: true
+}
+
+const check = v.compile(schema)
+
+module.exports = check

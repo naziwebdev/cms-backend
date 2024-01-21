97267fa4c1ee72868eed98bcd80a57337bea7267
nazi <nazipaeez777@gmail.com>
2024-01-21 22:48:43 +0330

    Create costs router

---
+++ routes/v1/cost.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const costController = require('../../controllers/v1/cost')
+const authMiddleware = require('../../middlewares/auth')
+const isAdminMiddleware = require('../../middlewares/isAdmin')
+
+const router = express.Router()
+
+
+router.route('/')
+    .post(authMiddleware, isAdminMiddleware, costController.create)
+    .get(authMiddleware, isAdminMiddleware, costController.getAll)
+
+router.route('/low-price')
+    .get(authMiddleware, isAdminMiddleware, costController.getLowPrice)
+
+router.route('/high-price')
+    .get(authMiddleware, isAdminMiddleware, costController.getHighPrice)
+
+router.route('/:id')
+    .put(authMiddleware, isAdminMiddleware, costController.editNote)
+    .delete(authMiddleware, isAdminMiddleware, costController.remove)
+
+module.exports = router

0eaa17038f4aa9c653084b05ce0c78f8395f9c1c
nazi <nazipaeez777@gmail.com>
2024-01-21 20:31:11 +0330

    Develope off validator

---
+++ validators/v1/off.js
@@ -0,0 +1,43 @@
+const validator = require('fastest-validator')
+
+const v = new validator()
+
+const schema = {
+
+    code: {
+        type: 'string',
+        min: 3
+    },
+    percent: {
+        type: 'number',
+        min:0
+    },
+    product: {
+        type: 'string',
+        pattern: /^[0-9a-fA-F]{24}$/,
+        optional: true
+    },
+    expireDay: {
+        type: 'number',
+        min:1
+    },
+    maxUsage: {
+        type: 'number',
+        min: 1
+    },
+    countUsaged: {
+        type: 'number',
+        min: 0
+    },
+    user: {
+        type: 'string',
+        pattern: /^[0-9a-fA-F]{24}$/,
+        optional: true
+    },
+    $$strict: true
+
+}
+
+const check = v.compile(schema)
+
+module.exports = check
